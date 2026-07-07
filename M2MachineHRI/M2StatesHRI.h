4a7a3b5db7a0b1576d35e099a9e159f147bc746d
YL13897 <alexylin3@gmail.com>
2026-07-07 21:52:06 +1000

    Change to VM2 A{0.32, 0.20} for X-Axis experiment.

--- M2MachineHRI/M2StatesHRI.h
+++ M2MachineHRI/M2StatesHRI.h
@@ -116,7 +116,8 @@ class M2ProbMoveState : public M2TimedState {
         bool isFinished() const { return finishedFlag; }
         
         // --- Experiment config ---
-        VM2 A{0.32, 0.25}; // Target A position (m)
+        VM2 A{0.32, 0.20}; // Target A position (m) - X-Axis
+        // VM2 A{0.50, 0.25}; // Target A position (m) - Y-Axis
         int activeAxis_ = 0; // 0 = X Axis, 1 = Y Axis.
         bool safetyTripped = false;
         
