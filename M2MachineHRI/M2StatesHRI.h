73403dfdc829a04713d5b45f8beaa22f7ab1d18f
YL13897 <alexylin3@gmail.com>
2026-07-07 21:12:03 +1000

    change Axis back to X

--- M2MachineHRI/M2StatesHRI.h
+++ M2MachineHRI/M2StatesHRI.h
@@ -117,7 +117,7 @@ class M2ProbMoveState : public M2TimedState {
         
         // --- Experiment config ---
         VM2 A{0.32, 0.30}; // Target A position (m)
-        int activeAxis_ = 1; // 0 = X Axis, 1 = Y Axis.
+        int activeAxis_ = 0; // 0 = X Axis, 1 = Y Axis.
         bool safetyTripped = false;
         
         // --- Workspace limits and wall config ---
